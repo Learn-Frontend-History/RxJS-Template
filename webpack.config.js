72cb287bb8a6848bf0ccc644177dd015c2a28341
KovalenkoAV <merlinwt@gmail.com>
2021-07-24 21:21:03 +0300

    Add rxjs entry
     - add script into `rxjs` folder to auto include into rxjs-entry

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
+const fs = require('fs')
 const path = require('path');
+
 const dir = (__path) => path.resolve(__dirname, __path);
 
 const plugins = {
@@ -6,17 +8,35 @@ const plugins = {
     css: require('mini-css-extract-plugin'),
 };
 
+function getResolveExtensions() {
+    return ['ts', 'js']
+}
+function getRxJSEntry() {
+    const extensions = getResolveExtensions().join('|')
+    const rxjs = fs.readdirSync(
+        'src/rxjs'
+    ).filter(
+        file => new RegExp(`.(${extensions})$`).test(file)
+    ).map(file => `./rxjs/${file}`)
+
+    return rxjs.length ? { rxjs } : {}
+}
+
 module.exports = {
     mode: 'development',
     context: dir('src'),
-    entry: ['./htmlLog/index.js', './index.ts'],
+    entry: {
+        main: './index.ts',
+        log: './htmlLog/index.js',
+        ...getRxJSEntry()
+    },
     output: {
         path: dir('dist'),
         filename: '[name].js',
         clean: true
     },
     resolve: {
-        extensions: ['.js', '.ts']
+        extensions: getResolveExtensions().map(ext => `.${ext}`)
     },
     plugins: [
         new plugins.html({template: 'index.html'}),
