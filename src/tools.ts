f258a4ca6fe12b351d2b7a015b48052556718ca6
KovalenkoAV <merlinwt@gmail.com>
2021-07-25 18:43:56 +0300

    Add tools lib

---
+++ src/tools.ts
@@ -0,0 +1,37 @@
+type Source = 'Native' | 'RxJS'
+
+export function print(source: Source, value: any) {
+    console.log(`${source}: ${value}`)
+}
+
+interface Options {
+    header: string,
+    buttons: {
+        id: string,
+        caption: string
+    }[]
+}
+
+export function prepareDOM(options: Options) {
+    const parser = new DOMParser()
+    document.getElementById('container').append(
+        parser.parseFromString(`
+            <div>
+                <h1> ${options.header} </h1>
+                ${
+                    options.buttons.reduce(
+                        (htmlButtons, buttonMeta) => {
+                            htmlButtons.push(`<button id="${buttonMeta.id}"> ${buttonMeta.caption} </button>`)
+                            
+                            return htmlButtons
+                        },
+                        []
+                    ).join('\n')
+                }
+            </div>`,
+            'text/html'
+        ).body.firstChild
+    )
+}
+
+
