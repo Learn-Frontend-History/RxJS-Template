5801a45c668e35e4e9161d1136fd28cbbd12cbb7
KovalenkoAV <merlinwt@gmail.com>
2021-07-27 10:06:01 +0300

    Implement themes (dark and light)

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import './index.css'
+
+document.body.className = localStorage.getItem('theme') || 'light'
+
+document.getElementById('toggleTheme').addEventListener('click', _ => {
+    const map = {
+        light: 'dark',
+        dark: 'light'
+    }
+
+    localStorage.setItem(
+        'theme',
+        document.body.className = map[document.body.className]
+    )
+})
