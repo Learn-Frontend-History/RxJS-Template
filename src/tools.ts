20c5dff6ad18670c013ddc6cbb477166371d87fa
KovalenkoAV <merlinwt@gmail.com>
2021-08-02 11:16:55 +0300

    Add click into button option

--- src/tools.ts
+++ src/tools.ts
@@ -10,7 +10,8 @@ export interface Options {
     buttons: {
         id: string,
         caption: string,
-        title?: string
+        title?: string,
+        click?: (event: MouseEvent) => void
     }[]
 }
 
@@ -20,22 +21,40 @@ export function prepareDOM(options: Options) {
         parser.parseFromString(`
             <div>
                 <h1> ${options.header} </h1>
-                ${ options.description ? `<h3>${options.description}</h3>` : '' }
+                ${options.description ? `<h3>${options.description}</h3>` : ''}
                 ${
-                    options.buttons.reduce<string[]>(
-                        (htmlButtons, buttonMeta) => {
-                            htmlButtons.push(
-                                `<button id="${buttonMeta.id}" ${
-                                    buttonMeta.title ? `title="${buttonMeta.title}"` : ''
-                                } > ${buttonMeta.caption} </button>`)
-    
-                            return htmlButtons
-                        },
-                        []
-                    ).join('\n')
-                }
+                options.buttons.reduce<string[]>(
+                    (htmlButtons, buttonMeta) => {
+                        htmlButtons.push(
+                            `<button id="${buttonMeta.id}" ${
+                                buttonMeta.title ? `title="${buttonMeta.title}"` : ''
+                            } > ${buttonMeta.caption} </button>`)
+
+                        return htmlButtons
+                    },
+                    []
+                ).join('\n')
+            }
             </div>`,
             'text/html'
         ).body.firstChild
+    )
+
+    options.buttons.filter(
+        button => button.click instanceof Function
+    ).map(
+        meta => ({el: document.getElementById(meta.id), meta})
+    ).forEach(
+        button => button.el.addEventListener(
+            'click',
+            event => {
+                if (button.meta.title) {
+                    console.group()
+                    console.log(button.meta.title)
+                }
+
+                button.meta.click(event)
+            }
+        )
     )
 }
