0f612a807f6007f237d7bf89238f4ed7ca3c0e38
KovalenkoAV <merlinwt@gmail.com>
2021-08-30 18:58:40 +0300

    Refactoring
     Refactoring index.html: extract fragments to components

---
+++ src/components/header.ts
@@ -0,0 +1,6 @@
+import Header from '@/components/header/class'
+
+const header = new Header('RxJS - bla bla bla')
+document.getElementById('header').append(header.component)
+
+export default header
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,13 @@
 import './index.css'
+import header from "./components/header";
+import _log from './components/log'
+
+import holder from "./components/holder";
 
 document.body.className = localStorage.getItem('theme') || 'light'
 
-
-// LOG
-import _log from './components/log'
+document.getElementById('header').append(header.component)
 
 document.getElementById('log').append(_log.component)
 
-import Header from '@/components/header/class'
-
-const header = new Header('RxJS - bla bla bla')
-document.getElementById('header').append(header.component)
-
-// HOLDER
-import Holder from "@/components/holder/class";
-import Button from "@/components/button/class";
-
-const holder = new Holder()
-document.getElementById('cards-wrapper').append(holder.component)
-
-document.body.addEventListener(
-    'click',
-    _ => holder.show({
-        title: 'Some process...',
-        controls: [
-            new Button('Some action', _ => 'Some action')
-        ]
-    })
-)
+document.getElementById('holder').append(holder.component)
