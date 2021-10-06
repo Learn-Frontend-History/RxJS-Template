22c7f038990b6e2025c89d482e5a1bde23fe4255
KovalenkoAV <merlinwt@gmail.com>
2021-10-06 08:54:40 +0300

    Log template and controller

--- src/components/log/index.ts
+++ src/components/log/index.ts
@@ -2,7 +2,26 @@ import template from "./index.html"
 import {Templater} from "@/base/templater";
 
 export class LogController {
-    groups = []
+    staticRows = [{message: 41}, {message: 42}, {message: 43}]
+    get groups(): any[] {
+        const groups = []
+        for (let i = 1; i < 5; i++ ) {
+            const subGroups = []
+            for (let j = 1; j < 5; j++ ) {
+                const rows = []
+                for (let k = 1; k < 5; k++ ) {
+                     rows.push({message: `${i}.${j}.${k}`})
+                }
+
+                subGroups.push({rows, caption: `${i}.${j}`})
+            }
+
+            groups.push({subGroups, caption: `${i}`})
+
+        }
+
+        return groups
+    }
 }
 
 export const logController = new LogController()
