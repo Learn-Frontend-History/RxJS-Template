c91ef30e15b12788d8002671364337bee05e00c1
KovalenkoAV <merlinwt@gmail.com>
2021-11-01 15:06:16 +0300

    Refactoring animation component

--- src/common/components/animation/class.ts
+++ src/common/components/animation/class.ts
@@ -7,24 +7,6 @@ export default class Animation extends Component {
     constructor() {
         super(svg);
     }
-
-    show() {
-        this.set('display', true)
-    }
-
-    hide() {
-        this.set('display', false)
-    }
-
-    set(name, value) {
-        switch (name) {
-            case 'display':
-                this.component.style.display = value ? 'block' : 'hide'
-                break
-            default:
-                super.set(name, value)
-        }
-    }
 }
 
 Factory.reg('animation', Animation)
